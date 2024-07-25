9f48a0302b010c08fc971095d8161096b31670f4
surendrasinghsodh <surendradjangotest@gmail.com>
2024-07-25 21:51:39 +0530

    Add certificate

--- myapp/views.py
+++ myapp/views.py
@@ -14,21 +14,24 @@ def contact_view(request):
     subjects = request.POST.get('subject')
     messages = request.POST.get('message')
 
-    subject = "Thank you for contacting us"
-    message = 'Hello, ' + name + '\n\n' + ('I have received your message and will get back to you soon.'
-                                           '\n\nBest regards\nSurendrasingh Sodha')
-    email_from = settings.EMAIL_HOST_USER
-    recipient_list = [email, ]
-    send_mail(subject, message, email_from, recipient_list)
+    try:
+        subject = "Thank you for contacting us"
+        message = 'Hello, ' + name + '\n\n' + ('I have received your message and will get back to you soon.'
+                                               '\n\nBest regards\nSurendrasingh Sodha')
+        email_from = settings.EMAIL_HOST_USER
+        recipient_list = [email, ]
+        send_mail(subject, message, email_from, recipient_list)
 
-    subject1 = 'Contact from Website'
-    message1 = ('Hello Surendrasingh, ' + '\n\n' + request.POST['name'] + ' want to contact you.' + '\n\n' + \
-                'Name : ' + name + '\n' +
-                'Email : ' + email + '\n' +
-                'Phone : ' + phone + '\n' +
-                'Subject : ' + subjects + '\n' +
-                'Message : ' + messages)
-    recipient_list1 = ['surendradjangotest@gmail.com', ]
-    send_mail(subject1, message1, email_from, recipient_list1)
+        subject1 = 'Contact from Website'
+        message1 = ('Hello Surendrasingh, ' + '\n\n' + request.POST['name'] + ' want to contact you.' + '\n\n' +
+                    'Name : ' + name + '\n' +
+                    'Email : ' + email + '\n' +
+                    'Phone : ' + phone + '\n' +
+                    'Subject : ' + subjects + '\n' +
+                    'Message : ' + messages)
+        recipient_list1 = ['surendradjangotest@gmail.com', ]
+        send_mail(subject1, message1, email_from, recipient_list1)
+    except:
+        print("Data not Found in database")
 
     return redirect('/')
